0e525f428a6a58543c0b2ae0a0e9b98eed73ffbc
Корховой Вадим <veider@inbox.ru>
2023-04-09 12:19:09 +0300

    Добавлены заготовка для виджета, бутап ресивера и сервиса

---
+++ app/src/main/java/ru/geekbrains/eventsreminder/service/BootUpReceiver.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.eventsreminder.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootUpReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
