8755b505bddb849a57caea45b797885773f1c536
Vladimir Petrov <vl.pva@yandex.ru>
2023-04-19 22:12:04 +0300

    добавил interface UseCases

--- app/src/main/java/ru/geekbrains/eventsreminder/usecases/UseCasesImpl.kt
+++ app/src/main/java/ru/geekbrains/eventsreminder/usecases/UseCasesImpl.kt
@@ -4,8 +4,8 @@ import ru.geekbrains.eventsreminder.domain.EventData
 import ru.geekbrains.eventsreminder.domain.ResourceState
 import ru.geekbrains.eventsreminder.repo.Repo
 
-class UseCases (val repo:Repo){
-    suspend fun loadListEvent():ResourceState<List<EventData>>{
+class UseCasesImpl (val repo:Repo):UseCases{
+    override suspend fun loadListEvent():ResourceState<List<EventData>>{
         val loadResponse = repo.loadData()
         when(loadResponse){
             is ResourceState.SuccessState -> {
